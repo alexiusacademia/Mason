848d25ef3cc31e4c441739a9b1e087803fb3be9e
Alexius Academia <alexius.sayco.academia@gmail.com>
2023-10-20 13:39:29 -0700

    Fixed basic messages

--- Mason/Views/WeeklyView.swift
+++ Mason/Views/WeeklyView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct WeeklyView: View {
+    @Environment(\.modelContext) private var modelContext
     @Query private var tasks: [Task]
     
     @State private var items: [Task] = []
@@ -18,11 +19,15 @@ struct WeeklyView: View {
             ZStack {
                 Color.green.opacity(0.3).ignoresSafeArea()
                 
-                List {
-                    ForEach(items) { task in
-                        TaskRow(task: task, showDate: true)
-                    }
-                }.scrollContentBackground(.hidden)
+                if items.count == 0 {
+                    Text("No tasks for this week.")
+                } else {
+                    List {
+                        ForEach(items) { task in
+                            TaskRow(task: task, showDate: true)
+                        }.onDelete(perform: deleteItems)
+                    }.scrollContentBackground(.hidden)
+                }
             }
             .toolbar {
                 ToolbarItem {
@@ -31,17 +36,30 @@ struct WeeklyView: View {
             }
             .navigationTitle("Weekly")
         }.onAppear() {
-            items = []
-            
-            let currentDate = Date.now
-            let currentWeek = Calendar.current.component(.weekOfYear, from: currentDate)
-            
-            for task in tasks {
-                if Calendar.current.component(.weekOfYear, from: task.timestamp) == currentWeek {
-                    items.append(task)
-                }
+            updateItems()
+        }
+    }
+    
+    private func updateItems() {
+        items = []
+        
+        let currentDate = Date.now
+        let currentWeek = Calendar.current.component(.weekOfYear, from: currentDate)
+        
+        for task in tasks {
+            if Calendar.current.component(.weekOfYear, from: task.timestamp) == currentWeek {
+                items.append(task)
             }
         }
-        
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                let task = (items[index])
+                
+                modelContext.delete(task)
+            }
+        }
     }
 }
