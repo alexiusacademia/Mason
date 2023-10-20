fcc4ad38ead66f7a2b99897608c15975f65f9bd9
Alexius Academia <alexius.sayco.academia@gmail.com>
2023-10-20 07:35:09 -0700

    Created placeholder files for alerts and forms

---
+++ Mason/Views/Alerts.swift
@@ -0,0 +1,18 @@
+//
+//  Alerts.swift
+//  Mason
+//
+//  Created by Alexius Academia on 10/20/23.
+//
+
+import SwiftUI
+
+struct Alerts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Alerts()
+}
---
+++ Mason/Views/Forms.swift
@@ -0,0 +1,14 @@
+//
+//  Forms.swift
+//  Mason
+//
+//  Created by Alexius Academia on 10/20/23.
+//
+
+import SwiftUI
+
+struct Forms: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
